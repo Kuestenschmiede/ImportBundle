48dba3007703b85caee55bb7b1df6de45deb9c4f
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2017-08-24 14:15:52 +0200

    replace copyright

--- Resources/contao/config/config.php
+++ Resources/contao/config/config.php
@@ -1,34 +1,12 @@
 <?php
 
 /**
- * Contao Open Source CMS
- * Copyright (C) 2005-2011 Leo Feyer
- *
- * Formerly known as TYPOlight Open Source CMS.
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- *
- * @package     con4gis
- * @filesource  config.php
- * @version     1.0.0
- * @since       13.04.17 - 11:09
- * @author      Patrick Froch <info@easySolutionsIT.de>
- * @link        http://easySolutionsIT.de
- * @copyright   e@sy Solutions IT 2017
- * @license     EULA
+ * con4gis
+ * @version   php 7
+ * @package   con4gis
+ * @author    con4gis authors (see "authors.txt")
+ * @copyright Küstenschmiede GmbH Software & Design 2017
+ * @link      https://www.kuestenschmiede.de
  */
 /**
  * BACK END MODULES
