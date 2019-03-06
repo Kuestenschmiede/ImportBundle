8345c19977f8ed13ddce973a6fb3456161171f92
coastforge-cro <claudio.ross@kuestenschmiede.de>
2019-03-06 11:36:00 +0100

    Sort backend modules

--- Resources/contao/config/config.php
+++ Resources/contao/config/config.php
@@ -20,6 +20,9 @@ $GLOBALS['BE_MOD']['con4gis'] = array_merge($GLOBALS['BE_MOD']['con4gis'], array
     )
 ));
 
+$GLOBALS['BE_MOD']['con4gis'] =
+    \con4gis\CoreBundle\Resources\contao\classes\C4GUtils::sortBackendModules($GLOBALS['BE_MOD']['con4gis']);
+
 /**
  * Importsettings
  */
