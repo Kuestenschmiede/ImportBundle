91fe775d548b2ab1a755bf2cf0a5ccecce31eed3
coastforge-cro <claudio.ross@kuestenschmiede.de>
2019-03-11 13:01:30 +0100

    Fix backend strucutre

--- Resources/contao/config/config.php
+++ Resources/contao/config/config.php
@@ -12,12 +12,14 @@
  */
 
 $GLOBALS['con4gis']['import']['installed'] = true;
-array_insert($GLOBALS['BE_MOD'], array_search('con4gis_core', array_keys($GLOBALS['BE_MOD'])) + 1, array(
+array_insert($GLOBALS['BE_MOD']['con4gis_core'], array_search('con4gis_core', array_keys($GLOBALS['BE_MOD'])) + 1, array(
     'import' => array(
         'tables'        => array('tl_c4g_import'),
         'runimport'     => array('\con4gis\ImportBundle\Classes\Contao\Modules\ModulImport', 'runImport')
     )
 ));
+
+
 
 /**
  * Importsettings
